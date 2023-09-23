aa1d30631332acc211044cf74c0c46ac5f93999c
nasimrifat101 <nasimrifat101@gmail.com>
2023-09-24 00:26:10 +0600

    404 page added

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,27 @@
 import { Link } from "react-router-dom";
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 const ErrorPage = () => {
   return (
-    <div>
+    <div className="grid grid-cols-2 gap-20 m-20 ml-40">
       <div>
-        <h3>404 page not found</h3>
-        <p>We are sorry but the page you are looking for does not exist.</p>
-        <Link to='/'>Go back to home</Link>
+        <div className="text-7xl font-bold bg-gradient-to-r from-indigo-500 via-purple-400 to-pink-300 ... bg-clip-text text-transparent">
+          BOO~ <br />
+          CONTENTS <br />
+          RAN <br />
+          AWAY
+        </div>
+        <h2 className=" mt-10 font-semibold">DON'T WORRY I CAN HELP YOU</h2>
+        <div>
+          <Link to="/">
+            <p className="flex items-center text-sm font-semibold text-green-400">
+              <AiOutlineArrowRight></AiOutlineArrowRight> FIND YOUR WAY HOME
+            </p>
+          </Link>
+        </div>
+      </div>
+      <div>
+        <img className="h-96" src="/src/assets/images/ghost.png" alt="" />
       </div>
     </div>
   );
